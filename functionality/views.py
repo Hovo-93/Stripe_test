5b866532106fead90a8fad2db4598a869debcd06
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-11-27 19:14:50 +0300

    added

--- functionality/views.py
+++ functionality/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .serializers import ItemSerializer
 from .models import Item
 from rest_framework import viewsets, status
@@ -10,13 +11,8 @@ from rest_framework.response import Response
 
 
 
-class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
 
 
-
-@api_view(['GET'])
 def buy(request, pk):
     item = Item.objects.get(id=pk)
     stripe.api_key = settings.STRIPE_API_KEY
@@ -35,7 +31,18 @@ def buy(request, pk):
         success_url='http://google.com',
         cancel_url='http://facebook.com',
     )
-    return Response({'payment_link': session.url})
-    # return redirect(session.url, code=303)
+    return JsonResponse({'session_id': session.id})
+
 
 # Create your views here.
+def item_detail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    # Replace item details and descriptions accordingly
+    context = {
+        'item_name': item.name,
+        'item_description': item.description,
+        'item_price': item.price,
+        'publishable_key': 'pk_test_51OGlf1EQj5G4dxj6XtLjrPcmqXmGfQbcMlg4hGPIAcO4qG2A59lFNZeVyJRpsx86fJRtwD0tU1FSlvxnWVgPscDg00ijnJ4uaJ',
+        'item_id': item_id,
+    }
+    return render(request, 'temp.html', context)
