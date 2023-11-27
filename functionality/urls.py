dd45f70adb312100221fc0dc91845d284aab8ca3
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-11-27 19:15:21 +0300

    added item/url/

--- functionality/urls.py
+++ functionality/urls.py
@@ -3,14 +3,15 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register(r'item', views.ItemViewSet)
+# router = DefaultRouter()
+# router.register(r'item', views.ItemViewSet)
 
 
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    path('buy/<int:pk>', views.buy),
+    # url(r'^', include(router.urls)),
+    path('buy/<int:pk>/', views.buy),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
 
 ]
